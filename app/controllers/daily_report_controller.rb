cb6beabd231cb49810d413be169a0c29b5a7b76c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-06-23 22:24:43 -0700

    developing daily report routes

--- app/controllers/daily_report_controller.rb
+++ app/controllers/daily_report_controller.rb
@@ -4,12 +4,12 @@ class ApplicationController < Sinatra::Base
 
     get '/daily_reports' do
         # @daily_reports = DailyReport.all
-        "this is the daily report index"
-        erb :'/daily_reports/index'
+        # "this is the daily report index"
+        erb :'daily_reports/index'
     end
 
     get '/daily_reports/new' do
-        erb :'/daily_reports/new'
+        erb :'daily_reports/new'
     end
 
     get '/daily_reports/:id' do
@@ -18,5 +18,19 @@ class ApplicationController < Sinatra::Base
 
     post '/daily_reports' do
         
+    end
+
+    get '/daily_reports/:id/edit' do
+        erb :'daily_reports/edit'
+    end
+ 
+    post '/daily_reports/:id' do
+        @daily_report = [] 
+        redirect "/daily_reports/#{@daily_report.id}"
+        # erb :'daily_reports/show'
+    end
+
+    delete '/daily_reports/:id' do
+        redirect '/daily_reports'
     end
 end

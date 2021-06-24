96fffc700596c0c662b14ff32dd2acefdb013e4a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-06-23 21:11:14 -0700

    develop daily report routes

--- app/controllers/daily_report_controller.rb
+++ app/controllers/daily_report_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < Sinatra::Base
         "this is the daily report index"
         erb :'/daily_reports/index'
     end
+
+    get '/daily_reports/new' do
+        erb :'/daily_reports/new'
+    end
+
+    get '/daily_reports/:id' do
+        erb :'daily_reports/show'
+    end
+
+    post '/daily_reports' do
+        
+    end
 end

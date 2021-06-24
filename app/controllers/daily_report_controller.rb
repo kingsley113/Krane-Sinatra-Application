3695bda92219b8c7c15131aec43988f039a83eab
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-06-24 13:16:24 -0700

    develop edit routes

--- app/controllers/daily_report_controller.rb
+++ app/controllers/daily_report_controller.rb
@@ -3,39 +3,55 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
     get '/daily_reports' do
-        # @daily_reports = DailyReport.all
+        @daily_reports = DailyReport.all
         # "this is the daily report index"
+        # TODO: add login validation
         erb :'daily_reports/index'
     end
 
     get '/daily_reports/new' do
+        # TODO: add login validation
         erb :'daily_reports/new'
     end
 
     get '/daily_reports/:id' do
+        # TODO: add login validation
         erb :'daily_reports/show'
     end
 
     post '/daily_reports' do
+        # TODO: add validation
         daily_report = DailyReport.new
         params[:daily_report].each do |attribute, value|
-            # binding.pry
             daily_report[:"#{attribute}"] = value
         end
         daily_report.save
     end
 
     get '/daily_reports/:id/edit' do
+        @daily_report = DailyReport.find(params[:id])
+
+        # TODO: add login and user validation
+
         erb :'daily_reports/edit'
     end
  
     post '/daily_reports/:id' do
-        @daily_report = [] 
+        # TODO: add validation
+        @daily_report = DailyReport.find(params[:id])
+
+        params[:daily_report].each do |attribute, value|
+            daily_report[:"#{attribute}"] = value
+        end
+        daily_report.save
+
         redirect "/daily_reports/#{@daily_report.id}"
-        # erb :'daily_reports/show'
     end
 
     delete '/daily_reports/:id' do
+        @daily_report = DailyReport.find(params[:id])
+        # TODO: add validation
+        @daily_report.delete
         redirect '/daily_reports'
     end
 end

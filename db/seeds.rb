fc5ed5de2b9470917b78b7b0a24f086126c53cb2
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-06-25 12:01:58 -0700

    develop seed file, create users

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# Generate Users
+cameron = User.create{first_name: "Cameron", last_name: "Kingsley", username: "kingsley113", password: "12345", email: "cameron@mail.com", position: "Owner"}
+jimmy = User.create{first_name: "Jimmy", last_name: "Diresta", username: "jDiresta", password: "12345", email: "jimmy@mail.com", position: "Superintendent"}
+bob = User.create{first_name: "Bob", last_name: "Clagett", username: "bClagett", password: "12345", email: "bob@mail.com", position: "Foreman"}
+david = User.create{first_name: "David", last_name: "Piccutio", username: "dPiccutio", password: "12345", email: "david@mail.com", position: "Project Manager"}
+thomas = User.create{first_name: "Thomas", last_name: "Brush", username: "tBrush", password: "12345", email: "thomas@mail.com", position: "Superintendent"}
+
+# Generate Projects

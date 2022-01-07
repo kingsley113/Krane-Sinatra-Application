05ca9556179c3fb132f925a1fbd858b14926e3ea
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-06 20:48:27 -0800

    Adding Heroku Specified postgres connection

--- config/environment.rb
+++ config/environment.rb
@@ -13,13 +13,16 @@ configure :development do
 end
 
 # ActiveRecord::Base.establish_connection(
-#     :adapter => "sqlite3",
-#     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
+	#     :adapter => "sqlite3",
+	#     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+	# )
+	
 
-ActiveRecord::Base.establish_connection(
-	:adapter => "postgresql",
-	:database => "db/#{ENV['SINATRA_ENV']}"
-)
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+# ActiveRecord::Base.establish_connection(
+# 	:adapter => "postgresql",
+# 	:database => "db/#{ENV['SINATRA_ENV']}"
+# )
 
 require_all 'app' 
